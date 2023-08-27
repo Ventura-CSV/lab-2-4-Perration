f2292c74f0b2419e7bd6964599fdc506e12718ed
Perration <yolandag2013@yahoo.com>
2023-08-26 19:02:17 -0700

    commit

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    original_str = "Python Programming"
+    sub1 = original_str[: -12]
+    sub2 = (original_str.split()[1])
+    merged_str = sub2 + sub1 
+    
+    print(sub2) 
+    print(sub1) 
+    print(merged_str)
+      
     pass
 
 
